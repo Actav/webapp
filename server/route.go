9a24ce7ae20c62fd96cb6b1fe931b03c62f101db
Actav <hp@actav.com>
2025-02-12 11:26:20 +0300

    ADD: routeStaticFiles in server/route.go

--- server/route.go
+++ server/route.go
@@ -11,8 +11,7 @@ func (s *server) Routes() {
 	s.mux.HandleFunc("/", s.handleIndex)
 
 	// Статические файлы (CSS, JS)
-	s.mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-	s.mux.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
+	s.routeStaticFiles()
 }
 
 // handleIndex обрабатывает запросы на главную страницу.
@@ -21,9 +20,17 @@ func (s *server) handleIndex(w http.ResponseWriter, r *http.Request) {
 
 	// Логирование запроса
 	s.logger.Printf("Запрос на главную страницу от %s", r.RemoteAddr)
+
 	// Выполняем шаблон "layout", внутри которого определён блок "content"
 	if err := s.renderer.Render(w, "layout", data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Println("Ошибка рендеринга шаблона:", err)
 	}
+}
+
+// Функция для роутинга статических файлов
+func (s *server) routeStaticFiles() {
+	s.mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	s.mux.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
+	s.mux.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 }
